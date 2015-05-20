be7520df50b0eeb0389dbf4eb463619c35dcbc60
laurencemeynell <laurence.meynell@gmail.com>
2015-05-20 14:03:33 +0100

    Updated WeightsCalc.java
    Added main method which calls runCalc() the method which will run the
    calculator.
    Added a userInput() method to read from the command line.
    Added sets & gets for instance attributes.
    Added runCalc() stub which will eventually ask the user to input their
    bar and weights and a then perform calculations on them until the user
    enters nothing for a target.

--- WeightsCalc.java
+++ WeightsCalc.java
@@ -30,7 +30,7 @@ public class WeightsCalc
       this(0.0, new ArrayList<Weights>());
       //availableWeights = new TreeMap<>();
    }
-   
+
    /**
     * Two argument contructor for objects of class WeightsCalc
     * @param aBar the weight of your bar
@@ -42,7 +42,78 @@ public class WeightsCalc
       this.availableWeights = anAvailableWeights;
    }
    
+   /**
+    * getBarWeight returns the weight of the bar
+    * @return the weight of the bar
+    */
+   public double getBarWeight()
+   {
+      return this.barWeight;
+   }
    
+   /**
+    * getAvailableWeights returns a List of all the available weights
+    * @return all the available weights as a List
+    */
+   public List<Weights> getAvailableWeights()
+   {
+      return this.availableWeights;
+   }
+   
+   /**
+    * Sets the weight of the bar to the parameter
+    * @param the weight of the bar to be set
+    */
+   public void setBarWeight(double aBarWeight)
+   {
+      this.barWeight = aBarWeight;
+   }
 
+   /**
+    * userInput gets input from the user from the command line
+    * It returns this input as a String
+    * @return String whatever the user just inputted
+    */
+   public String userInput()
+   {
+      Scanner aScanner = new Scanner(System.in);
+      String theInput = aScanner.nextLine();
+      return theInput;
+   }
+
+   /**
+    * runCalc asks the user to input their bar weight and available weights
+    * Then it will asks the user to input a target weight and display how to
+    * load the side of a barbell to achieve this target weight repeatedly
+    * until the user enters an empty String for a target weight
+    */
+   public void runCalc()
+   {
+      //this.getWeights();
+      System.out.println(this.toString());
+   }
+   
+   /**
+    * toString returns the weight of the bar followed by the weight and number
+    * of each set of plates
+    * @return the weight of the bar and number and weight of the plates
+    */
+   @Override
+   public String toString()
+   {
+      String returnString = null;
+      returnString += "Bar: " + this.getBarWeight();
+      
+      return returnString;
+   }
+
+   /**
+    * main method runs the WeightsCalc program in the command line
+    */
+   public static void main(String[] args)
+   {
+      WeightsCalc weightsCalc = new WeightsCalc();
+      weightsCalc.runCalc();
+   }
 
 }
